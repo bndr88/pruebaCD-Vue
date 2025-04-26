200939194fea271674f081b289aec72a5db48921
Wender Mendez <wendermendez@gmail.com>
2025-04-26 01:59:36 -0400

    agregando ejemplo base
    agregando ejemplo base

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import CarritoCompras from './components/CarritoCompras.vue'
+
+</script>
+
+<template>
+  <div >
+    <header style="text-align: center;">
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    </header>
+
+    <CarritoCompras />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CarritoCompras.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1 style="color: #41B883;text-align: center;">Actividad Nro. 4</h1>
+        <br>
+        
+        <h2 style="text-align: center;">Carrito de Compras:</h2>
+        <input v-model="nuevoItem" placeholder="Agregar Item" />
+       
+        <button @click="agregarAlCarrito">Agregar</button>
+
+        <ul>
+            <li v-for="(item, index) in carrito.carrito" :key="index">
+                {{ item }}
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Carrito } from '../stores/carrito'
+
+const nuevoItem = ref('')
+const carrito = Carrito()
+
+const agregarAlCarrito = () => {
+  carrito.agregarProductos(nuevoItem.value)
+  nuevoItem.value = ''
+}
+
+</script>
